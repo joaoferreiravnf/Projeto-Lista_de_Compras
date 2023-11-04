2f4d4672ef4f1a02a4c64ebd3dddc01d030a1a86
joaoferreiravnf <130000617+joaoferreiravnf@users.noreply.github.com>
2023-11-04 20:55:41 +0000

    atual

--- __init__.py
+++
@@ -1,8 +0,0 @@
-# python -m flask run
-
-from flask import Flask, jsonify
-from backend import update_item, insert_item, delete_item, show_list
-from routes import routes
-
-app = Flask(__name__)
-app.config.from_object('config')
